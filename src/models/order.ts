66c208056d603e15f48e05365bc6af091abf3c4f
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-09-27 19:09:32 +0300

    order model

---
+++ src/models/order.ts
@@ -0,0 +1,43 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface ORDER extends Document {
+  memberID: Schema.Types.ObjectId;
+  productID: Schema.Types.ObjectId;
+  collectionTime: Date;
+  paymentMethod: string;
+  status: string;
+}
+
+const orderSchema = new Schema<ORDER>(
+  {
+    memberID: {
+      type: Schema.Types.ObjectId,
+      ref: 'Member',
+      required: true,
+    },
+    productID: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    collectionTime: {
+      type: Date,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'accepted', 'rejected', 'fulfilled'],
+      default: 'pending',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
